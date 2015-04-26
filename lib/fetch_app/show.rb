59a4ed0259d7b4222c002488404cf8b8b9a10a19
Will <yh@fir.im>
2015-04-26 23:35:14 +0800

    修改show table 间距

--- lib/fetch_app/show.rb
+++ lib/fetch_app/show.rb
@@ -13,25 +13,34 @@ class Show
   def run
     table(:border => true) do
      row do
-       column('应用名称', :width => 20)
-       column("#{@name}", :width => 50)
+       column("微信", :width => 40)
+       column("com.weuxin.me.name", :width => 30, :align => 'right', :padding => 5)
+       column("聊天社交", :width => 15)
      end
-     row do
-       column('应用ID')
-       column("#{@app_id}")
-     end
-     row do
-       column('应用类别')
-       column("#{@gener}")
+     row :color => 'red' do
+        column '微信'
+        column 'com.pernanana.name'
+        column '社交聊天', :color => 'blue'
      end
      # row do
-     #   column('应用链接')
-     #   column("#{@link}")
+     #   column('应用ID')
+     #   column("#{@app_id}",:width => 30, :align => 'center')
+     #   column('√', :width => 15)
      # end
-     row do
-       column('应用版本')
-       column("#{@version}")
-     end     
+     # row do
+     #   column('应用类别')
+     #   column("#{@gener}",:width => 30, :align => 'center')
+     #   column('√', :width => 15)
+     # end
+     # # row do
+     # #   column('应用链接')
+     # #   column("#{@link}")
+     # # end
+     # row do
+     #   column('应用版本')
+     #   column("#{@version}",:width => 30, :align => 'center')
+     #   column('√', :width => 15)
+     # end     
    end
   end
 end
